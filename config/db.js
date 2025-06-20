65395120baf60468ec5697ad9119da7cf8e2a121
hasharas <hasharamsankalpam@gmail.com>
2025-06-20 23:55:44 +0530

    Create:data base schema

--- config/db.js
+++ config/db.js
@@ -1,21 +1,3 @@
-// const mongoss = require('mongoose');
-
-
-
-
-// const connectDb = async () => {
-//       try {
-//             await mongoss.connect(process.env.MONGO_URL);
-//             console.log("Connect mongodb Data base.");
-//       } catch (error) {
-//             console.error("Faild to connect data base..!");
-//             process.exit(1);
-//       }
-// }
-
-// module.exports = connectDb;
-
-
 
 const mongoose = require('mongoose');
 
---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userModel = new mongoose.Schema({
+      name: { type: String, require: true },
+      email: { type: String, require: true },
+      age: { type: String, require: true }
+});
+
+
+
+module.exports = mongoose.model('User', userModel);
+
+
